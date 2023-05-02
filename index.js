3e11be3b7d99703d374f0b9354f37ddd19a109f1
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-02 22:42:51 +0600

    Cors added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
-// var cors = require("cors");
+var cors = require("cors");
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
+
+const chef = require("./data/chef.json");
 
 app.get("/", (req, res) => {
   res.send("The port is running");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
 });
 
 app.listen(port, () => {
